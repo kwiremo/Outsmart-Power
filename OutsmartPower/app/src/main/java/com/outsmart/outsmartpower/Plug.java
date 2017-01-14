e33598f53d97c2dff30bdb9a1cb013aebe34f78a
Chrisrox8 <christian.wagner@eagles.oc.edu>
2017-01-14 17:29:59 -0600

    Adding Initial Smart Outlet and Plug classes

---
+++ OutsmartPower/app/src/main/java/com/outsmart/outsmartpower/Plug.java
@@ -0,0 +1,19 @@
+package com.outsmart.outsmartpower;
+
+/**
+ * Created by christian.wagner on 1/14/17.
+ *
+ * Description: This class represents a single electrical plug on one of the Smart Oulets.
+ *
+ * Fields:
+ *  - status: The status (on or off) of the electrical plug
+ *  - current: The current being used on the electrical plug
+ *  - voltage: The voltage being used on the electrical plug
+ *
+ *  Methods:
+ *  + calculatePower(): This function will calculate the power (voltage*current), and return it.
+ *  + getStatus(): Returns the status of the outlet
+ */
+
+public class Plug {
+}
