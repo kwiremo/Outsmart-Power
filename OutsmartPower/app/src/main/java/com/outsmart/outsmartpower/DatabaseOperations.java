520e138b109ffcca90e4fa09cb021df8306f98ec
Chrisrox8 <christian.wagner@eagles.oc.edu>
2017-01-16 16:18:44 -0600

    Adding DatabaseOperations class and Adding functionality to SmartOutlet class

---
+++ OutsmartPower/app/src/main/java/com/outsmart/outsmartpower/DatabaseOperations.java
@@ -0,0 +1,8 @@
+package com.outsmart.outsmartpower;
+
+/**
+ * Created by christian.wagner on 1/16/17.
+ */
+
+public class DatabaseOperations {
+}
--- OutsmartPower/app/src/main/java/com/outsmart/outsmartpower/SmartOutlet.java
+++ OutsmartPower/app/src/main/java/com/outsmart/outsmartpower/SmartOutlet.java
@@ -1,5 +1,14 @@
 package com.outsmart.outsmartpower;
 
+import android.support.annotation.NonNull;
+
+import org.json.JSONStringer;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 /**
  * Created by christian.wagner on 1/14/17.
  *
@@ -8,21 +17,73 @@ package com.outsmart.outsmartpower;
  * Description: This class is made to represent a smart outlet.
  *
  * Fields:
- * - List<Plug>: A list of plugs associated with this Smart Outlet
- * - nickname: The name that the user will see for this outlet.
- * - ssid: The SSID broadcasted by this Smart Outlet
- * - password: The password to connect to the Smart Outlet's broadcasted SSID
- * - active: Boolean value that lets us know if this is the outlet communication with the android application
- * - sqlLite Database: The data associated with this smart outlet for storing power and status information
+ * - List<Plug> plugs: A list of plugs associated with this Smart Outlet
+ * - String nickname: The name that the user will see for this outlet.
+ * - String ssid: The SSID broadcasted by this Smart Outlet
+ * - String password: The password to connect to the Smart Outlet's broadcasted SSID
+ * - boolean active: True means this smart outlet is actively communicating with the android application
+ * - DatabaseOperations database: The power profile database associated with this smart outlet
  *
  * Methods:
  * + smartOutlet(String password, String ssid) - This is the constructor for the Smart Outlet Class
- * + setPassword(String password) - This sets the password for the Smart Outlet object
- * + setNickname(String name) - This sets the nickname for the Smart Outlet object
- * + addEntry(String JSONstring) - This adds an entry into the sqlLite database associated with this Smart Outlet
+ * + boolean addEntry(String JSONstring) - This adds an entry into the sqlLite database associated with this Smart Outlet
  * + getPresentEntry() - Gets the latest Power profile (voltage, current, etc) from the sqlLite database associated with this Smart Outlet
  *
  */
 
 public class SmartOutlet {
+
+    private List<Plug> plugs; //A list of the plugs associated with this Smart Outlet
+    private String nickname; //The name that the user will see for this outlet
+    private String ssid; //The SSID broadcasted by this Smart Outlet
+    private String password; //The password to connect to this Smart Outlet's broadcasted SSID
+    private boolean active; //True means this smart outlet is actively communicating with the android application
+    private DatabaseOperations database; //The database associated with this smart outlet
+
+    //Constructor for the SmartOutlet Class
+    public SmartOutlet(String initialPassword, String initialSsid){
+
+        //Set the initial ssid and password of this Smart outlet
+        setSsid(initialSsid);
+        setPassword(initialPassword);
+    }
+    //Adds power profile entry to the data base associated with this Smart Outlet
+    public boolean addEntry(String jsonString){
+        //Pass jsonString to DatabaseOperations class to test for
+
+        return false;
+    }
+
+    //Getter for the ssid field
+    public String getSsid() {
+        return ssid;
+    }
+    //Setter for the ssid field
+    public void setSsid(String ssid) {
+        this.ssid = ssid;
+    }
+    //Getter for the password field
+    public String getPassword() {
+        return password;
+    }
+    //Setter for the password field
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    //Getter for the nickname field
+    public String getNickname() {
+        return nickname;
+    }
+    //Setter for the nickname field
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+    //Getter for the active field
+    public boolean isActive() {
+        return active;
+    }
+    //Setter for the active field
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 }
