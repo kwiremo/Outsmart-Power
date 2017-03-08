d1256bb1a8aeed30bc79893843b4b12ab801f210
Chrisrox8 <christian.wagner@eagles.oc.edu>
2017-03-08 16:45:53 -0600

    Implemented UDPManager Class

--- OutsmartPower/app/src/main/java/com/outsmart/outsmartpower/managers/UDPManager.java
+++ OutsmartPower/app/src/main/java/com/outsmart/outsmartpower/managers/UDPManager.java
@@ -5,14 +5,19 @@ import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.outsmart.outsmartpower.PacketInformation;
+import com.outsmart.outsmartpower.Support.BootlLoader;
 import com.outsmart.outsmartpower.Support.Constants;
 import com.outsmart.outsmartpower.Support.ParentActivity;
 import com.outsmart.outsmartpower.UDPClient;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 import java.util.Observable;
+import java.util.Observer;
 
 /**
  * Created by Rene Moise on 1/14/2017.
@@ -31,7 +36,7 @@ import java.util.Observable;
  * Progress: the type of the progress units published during the background computation.
  * Result: the type of the result of the background computation.
  * */
-public class UDPManager extends Observable {
+public class UDPManager extends Observable implements Observer{
 
     //Instance of this singleton class
     private static UDPManager ourInstance = new UDPManager();
@@ -44,29 +49,30 @@ public class UDPManager extends Observable {
     private boolean serverRunning = false;      //for if server is running.
     private String packetReceived;
 
+    //The port to send frames from
+    private DatagramSocket sendSocket;
+
+    //The port to send frames from
+    private DatagramSocket receiveSocket;
+
     //Private class that receives packets
     private class receiveUDPPacket extends AsyncTask<Object, Object, Object> {
 
         @Override
         protected Object doInBackground(Object[] objects) {
 
-            //set serverRunning to true.
-            startServer();
+
 
             Log.e("TAG", "Server running");
 
             byte[] packetBuffer = new byte[Constants.BUFFER_SIZE];
             DatagramPacket dp = new DatagramPacket(packetBuffer, packetBuffer.length);
-            DatagramSocket ds = null;
-
             try {
-                ds = new DatagramSocket(Constants.SERVER_PORT);
-
                 while (serverRunning) {
-                    ds.receive(dp);
+                    receiveSocket.receive(dp);
                     packetReceived = new String(packetBuffer, 0, dp.getLength());
                     publishProgress(packetReceived);
-                    Thread.sleep(Constants.SERVER_SLEEP_TIME);
+                    //Thread.sleep(Constants.SERVER_SLEEP_TIME);
                 }
 
             } catch (Exception e) {
@@ -74,53 +80,37 @@ public class UDPManager extends Observable {
                         Toast.LENGTH_SHORT).show();
             } finally {
                 stopServer();
-                if (ds != null) {
-                    ds.close();
-                }
             }
-            return packetBuffer;
+            return null;
         }
 
         @Override
         protected void onProgressUpdate(Object... values) {
-            if (packetReceived == null);
+            if (values[0] != null){
+                String updatePacket = values[0].toString();
+                setChanged();
+                notifyObservers(values[0]);
+            };
         }
 
         @Override
         protected void onPostExecute(Object obj) {
-            //super.onPostExecute(result);
             stopServer();
         }
     }
 
     //Class to send UDP packets
-    private class sendUDPPacket extends AsyncTask<Object, Object, Object>{
-        //The packet that will be sent
-        String packetToSend;
+    private class sendUDPPacket extends AsyncTask<PacketInformation, Void, Void>{
+
+        String test;
 
         @Override
-        protected Object doInBackground(Object[] objects) {
-            //Get the first object to send in the list
-            if(objects[0] == null)
-                packetToSend = objects[0].toString();
-
-            DatagramSocket ds = null;
+        protected Void doInBackground(PacketInformation... arg0) {
+            PacketInformation pktInfo = arg0[0];
             try {
-                ds = new DatagramSocket();
-                DatagramPacket dp;
-
-                dp = new DatagramPacket(packetToSend.getBytes(), packetToSend.length(),
-                        InetAddress.getByName(Constants.getInstance().REMOTE_IP_ADDRESS),
-                        Constants.REMOTE_PORT);
-
-                ds.setBroadcast(true);
-                ds.send(dp);
-            } catch (Exception e) {
+                pktInfo.getSocket().send(pktInfo.getPacket());
+            } catch (IOException e) {
                 e.printStackTrace();
-            } finally {
-                if (ds != null) {
-                    ds.close();
-                }
             }
             return null;
         }
@@ -131,14 +121,36 @@ public class UDPManager extends Observable {
 
     }
 
-    //Method that sends a packet via the sendUDPPacket Class
-    public void sendPacket(){
+    @Override
+    public void update(Observable o, Object arg) {
+        //Once booted, initialize
+        if(o.getClass().equals(BootlLoader.class)){
 
+            startServer();
+
+            //TODO Add UDP client and server as observers to this
+        }
     }
 
-    //Method that recieves a packet via the receivedUDPPacketClass
-    public void receivePacket(){
+    //Method to setup sockets for receiving and sending
+    private void openSockets(){
+        //Open the sendSocket
+        try {
+            sendSocket = new DatagramSocket();
+        }
+        catch(SocketException e){
+            //Alert if socket failed to open
+            e.printStackTrace();
+        }
 
+        //Open the receiveSocket
+        try {
+            receiveSocket = new DatagramSocket(Constants.SERVER_PORT);
+        }
+        catch(SocketException e){
+            //Alert if socket failed to open
+            e.printStackTrace();
+        }
     }
 
     public boolean isServerRunning()
@@ -149,10 +161,35 @@ public class UDPManager extends Observable {
     public void stopServer()
     {
         serverRunning = false;
+        receiveSocket.close();
+        sendSocket.close();
     }
 
     private void startServer()
     {
-        serverRunning = false;
+        serverRunning = true;
+        openSockets();
+        new receiveUDPPacket().execute();
+    }
+
+    //Method to send packets
+    public void sendPacket(String packetToSend, String ipAddress){
+
+        InetAddress IPAddress;
+
+        //Try getting the ipAddress from the adjacency table using the frames destination address
+        try {
+            IPAddress = InetAddress.getByName(ipAddress);
+            //Create packet to send
+            DatagramPacket sendPacket = new DatagramPacket(packetToSend.getBytes(),
+                    packetToSend.length(),
+                    IPAddress,
+                    Constants.REMOTE_PORT);
+            //Send the frame
+            new sendUDPPacket().doInBackground(new PacketInformation(sendSocket,sendPacket));
+        }
+        catch(Exception e){
+            e.printStackTrace();//TODO implement exception
+        }
     }
 }
